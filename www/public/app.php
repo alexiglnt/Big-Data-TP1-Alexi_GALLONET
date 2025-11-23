7743ca38a9d71238bfa3a25fd66a6e24b03a821e
alexiglnt <alexigallonet@gmail.com>
2025-11-23 18:15:05 +0100

    Ajout pagination et systeme de recherche

--- www/public/app.php
+++ www/public/app.php
@@ -9,13 +9,53 @@ use Twig\Error\SyntaxError;
 $twig = getTwig();
 $manager = getMongoDbManager();
 
-// @todo implementez la récupération des données dans la variable $list
-// petite aide : https://github.com/VSG24/mongodb-php-examples
-$list = (array) getMongoDbManager()->selectCollection('tp')->find([])->toArray();
+$collection = $manager->selectCollection('tp');
+
+// ----- RECHERCHE -----
+$search = isset($_GET['q']) ? trim($_GET['q']) : '';
+
+$filter = [];
+if ($search !== '') {
+    // reecherche insensible à la casse sur plusieurs champs
+    $filter = [
+        '$or' => [
+            ['titre'  => ['$regex' => $search, '$options' => 'i']],
+            ['auteur' => ['$regex' => $search, '$options' => 'i']],
+            ['cote'   => ['$regex' => $search, '$options' => 'i']],
+        ],
+    ];
+}
+
+// ----- PAGINATION -----
+$perPage = 50;
+$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+if ($page < 1) {
+    $page = 1;
+}
+$skip = ($page - 1) * $perPage;
+
+$options = [
+    'limit' => $perPage,
+    'skip'  => $skip,
+    'sort'  => ['_id' => 1],
+];
+
+// cest la liste filtrée + paginée
+$cursor = $collection->find($filter, $options);
+$list = $cursor->toArray();
+
+// le total est filtré pour que la pagination tienne compte de la recherche
+$totalDocuments = $collection->countDocuments($filter);
+$totalPages = (int) ceil($totalDocuments / $perPage);
 
 // render template
 try {
-    echo $twig->render('index.html.twig', ['list' => $list]);
+    echo $twig->render('index.html.twig', [
+        'list'        => $list,
+        'currentPage' => $page,
+        'totalPages'  => $totalPages,
+        'search'      => $search,
+    ]);
 } catch (LoaderError|RuntimeError|SyntaxError $e) {
     echo $e->getMessage();
 }
